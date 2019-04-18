88fd4fb906ee12dbc0dd7d3ddb27f6f3c42e150b
SerialForBreakfast <joe@showblender.com>
2019-04-18 10:16:38 -0700

    Used StringConvertRegex to generate code from file listings

---
+++ SplitViewer/ImagesNames.swift
@@ -0,0 +1,39 @@
+//
+//  ImagesNames.swift
+//  SplitViewer
+//
+//  Created by Joseph McCraw on 4/18/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import UIKit
+//Generated this using REGEX and my StringConvertRegex Spreadsheet
+
+let B = UIImage(named: "B.jpg")!
+let aj = UIImage(named: "aj.jpg")!
+let dj = UIImage(named: "dj.jpg")!
+let ew = UIImage(named: "ew.jpg")!
+let jb = UIImage(named: "jb.jpg")!
+let lj = UIImage(named: "lj.jpg")!
+let mf = UIImage(named: "mf.jpg")!
+let p2 = UIImage(named: "p2.jpg")!
+let sb2 = UIImage(named: "sb2.jpg")!
+let tm = UIImage(named: "tm.jpg")!
+let aa = UIImage(named: "aa.jpg")!
+let bo = UIImage(named: "bo.jpg")!
+let dt = UIImage(named: "dt.jpg")!
+let hs = UIImage(named: "hs.jpg")!
+let kk = UIImage(named: "kk.jpg")!
+let m = UIImage(named: "m.jpg")!
+let nd = UIImage(named: "nd.jpg")!
+let pd = UIImage(named: "pd.jpg")!
+let ss = UIImage(named: "ss.jpg")!
+let ah = UIImage(named: "ah.jpg")!
+let cj = UIImage(named: "cj.jpg")!
+let dt1 = UIImage(named: "dt1.jpg")!
+let j = UIImage(named: "j.jpg")!
+let ld = UIImage(named: "ld.jpg")!
+let mc = UIImage(named: "mc.jpg")!
+let p = UIImage(named: "p.jpg")!
+let sb = UIImage(named: "sb.jpg")!
+let th = UIImage(named: "th.jpg")!
