af656c74a72f53c533abaa39ec5b378b3ed0bab7
SerialForBreakfast <joe@showblender.com>
2019-04-19 09:23:48 -0700

    Alphabetized Celebrity Photos

--- SplitViewer/ImagesNames.swift
+++ SplitViewer/ImagesNames.swift
@@ -8,32 +8,35 @@
 
 import UIKit
 //Generated this using REGEX and my StringConvertRegex Spreadsheet
+var names: [String]?
 
-let B = UIImage(named: "B.jpg")!
-let aj = UIImage(named: "aj.jpg")!
-let dj = UIImage(named: "dj.jpg")!
-let ew = UIImage(named: "ew.jpg")!
-let jb = UIImage(named: "jb.jpg")!
-let lj = UIImage(named: "lj.jpg")!
-let mf = UIImage(named: "mf.jpg")!
-let p2 = UIImage(named: "p2.jpg")!
-let sb2 = UIImage(named: "sb2.jpg")!
-let tm = UIImage(named: "tm.jpg")!
 let aa = UIImage(named: "aa.jpg")!
-let bo = UIImage(named: "bo.jpg")!
-let dt = UIImage(named: "dt.jpg")!
-let hs = UIImage(named: "hs.jpg")!
-let kk = UIImage(named: "kk.jpg")!
-let m = UIImage(named: "m.jpg")!
-let nd = UIImage(named: "nd.jpg")!
-let pd = UIImage(named: "pd.jpg")!
-let ss = UIImage(named: "ss.jpg")!
 let ah = UIImage(named: "ah.jpg")!
+let aj = UIImage(named: "aj.jpg")!
+let B = UIImage(named: "B.jpg")!
+let bo = UIImage(named: "bo.jpg")!
 let cj = UIImage(named: "cj.jpg")!
+let dj = UIImage(named: "dj.jpg")!
+let dt = UIImage(named: "dt.jpg")!
 let dt1 = UIImage(named: "dt1.jpg")!
+let ew = UIImage(named: "ew.jpg")!
+let hs = UIImage(named: "hs.jpg")!
 let j = UIImage(named: "j.jpg")!
+let jb = UIImage(named: "jb.jpg")!
+let kk = UIImage(named: "kk.jpg")!
 let ld = UIImage(named: "ld.jpg")!
+let lj = UIImage(named: "lj.jpg")!
+let m = UIImage(named: "m.jpg")!
 let mc = UIImage(named: "mc.jpg")!
+let mf = UIImage(named: "mf.jpg")!
+let nd = UIImage(named: "nd.jpg")!
 let p = UIImage(named: "p.jpg")!
+let p2 = UIImage(named: "p2.jpg")!
+let pd = UIImage(named: "pd.jpg")!
 let sb = UIImage(named: "sb.jpg")!
+let sb2 = UIImage(named: "sb2.jpg")!
+let ss = UIImage(named: "ss.jpg")!
 let th = UIImage(named: "th.jpg")!
+let tm = UIImage(named: "tm.jpg")!
+
+names = ["Bean","Angelina Jolie", "Danny Trejo", ""]
